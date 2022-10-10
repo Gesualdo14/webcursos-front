df2c4e527005e9b5de0d7a41eb222e77dcfa3ae7
Martín Gesualdo <mgesudev@gmail.com>
2022-10-10 13:46:05 -0300

    Cambios

--- next.config.js
+++ next.config.js
@@ -1,23 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["webcursos.blob.core.windows.net"],
-  },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: `style-src 'self' *.fontawesome.com fonts.cdnfonts.com`,
-          },
-        ],
-      },
-    ]
   },
 }
 
