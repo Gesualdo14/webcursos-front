08e85ae71a982fa690989cf45c9f4afd84e16878
Martín Gesualdo <mgesudev@gmail.com>
2022-10-10 15:03:10 -0300

    Cambios

--- components/CourseCard.js
+++ components/CourseCard.js
@@ -16,22 +16,28 @@ const CourseCard = ({ course }) => {
 
   return (
     <>
-      <div className="df jcsb cursorp card-container" onClick={handleClick}>
+      <div
+        className="df jcsb cursorp card-container posr"
+        onClick={handleClick}
+      >
         <div
           style={{
             boxShadow: "0 2px 5px rgba(0,0,0,0.2)",
-            width: `${155 * 1.77}px`,
+            width: `${15 * 1.77}rem`,
+            height: `15rem`,
+            overflow: "hidden",
           }}
         >
           <Image
             src={thumbnail}
             alt="Picture of the author"
-            width={155 * 1.77}
-            height={155}
+            width={`${15 * 1.77}rem`}
+            height={`15rem`}
+            layout="responsive"
           />
         </div>
-        <div className="df fdc card-body mt5">
-          <h3 className="cblack mv5">{name}</h3>
+        <div className="df fdc card-body">
+          <h3 className="cwhite mv5">{name}</h3>
           <p className="cgrey m0">{description}</p>
         </div>
       </div>
@@ -40,17 +46,19 @@ const CourseCard = ({ course }) => {
         {`
           .card-container {
             flex-direction: column;
-            background-color: white;
-            width: ${150 * 2 + "px"};
-            padding: 0.75rem;
+            background-color: var(--black);
+            width: ${15 * 1.77 + "rem"};
             border-radius: 0.5rem;
-            max-width: 20rem;
+            overflow: hidden;
+            box-shadow: 0 0 10px rgba(255, 255, 255, 0.1);
             transition: transform 0.2s ease;
             margin: 0 auto;
           }
 
           .card-body {
             width: 100%;
+            padding: 0.75rem;
+            background-color: var(--black);
           }
 
           .card-container:hover {
