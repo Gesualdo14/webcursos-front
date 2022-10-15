c1cca28629f5335500cadea49cefb2119d8a66a9
Martín Gesualdo <mgesudev@gmail.com>
2022-10-15 14:00:37 -0300

    Brain FAVICON

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const ContentSecPolicy = `default-src 'self' www.sandbox.paypal.com www.paypal.com ka-f.fontawesome.com localhost:4000 web-cursos.azurewebsites.net webcursos.blob.core.windows.net; script-src 'self' 'unsafe-eval' 'unsafe-inline' www.paypal.com kit.fontawesome.com; style-src 'self' 'unsafe-inline' *.fontawesome.com fonts.cdnfonts.com; font-src 'self' fonts.cdnfonts.com ka-f.fontawesome.com; img-src 'self' data: t.paypal.com`
+const ContentSecPolicy = `default-src 'self' www.sandbox.paypal.com www.paypal.com ka-f.fontawesome.com localhost:4000 web-cursos.azurewebsites.net webcursos.blob.core.windows.net; script-src 'self' 'unsafe-eval' 'unsafe-inline' www.paypal.com kit.fontawesome.com; style-src 'self' 'unsafe-inline' *.fontawesome.com fonts.cdnfonts.com; font-src 'self' fonts.cdnfonts.com ka-f.fontawesome.com; img-src 'self' data: t.paypal.com www.paypalobjects.com`
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
