477c5f7616a237c53810189cbe09886b86478219
Martín Gesualdo <mgesudev@gmail.com>
2023-08-03 08:59:17 -0300

    Cambios

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document"
+import Script from "next/script"
 
 export default function Document() {
   const PAYPAL_CLIENT_ID = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID
@@ -6,6 +7,19 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-F25DH8FPE3"
+        />
+        <Script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', 'G-F25DH8FPE3')`}
+          ;
+        </Script>
         <link
           href="https://fonts.cdnfonts.com/css/cubano?styles=103531,103532,103533,103534,103535"
           rel="stylesheet"
