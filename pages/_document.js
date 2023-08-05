d6b6f3585ea74b707c8312a3bf699a944455f09a
Martín Gesualdo <mgesudev@gmail.com>
2023-08-05 09:02:06 -0300

    Cambios

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,16 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <Script>
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K2FT6QHP');
+          `}
+        </Script>
+
         <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-F25DH8FPE3"
@@ -17,8 +27,8 @@ export default function Document() {
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             
-            gtag('config', 'G-F25DH8FPE3')`}
-          ;
+            gtag('config', 'G-F25DH8FPE3');
+            `}
         </Script>
         <link
           href="https://fonts.cdnfonts.com/css/cubano?styles=103531,103532,103533,103534,103535"
@@ -36,6 +46,15 @@ export default function Document() {
         <link rel="icon" href="/logo.png" />
       </Head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K2FT6QHP"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
